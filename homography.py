889776893a8ac063e9fbcecddeeefc3884b2d1a4
Janardhana Reddy M S <76699907+JANARDHANAREDDYMS@users.noreply.github.com>
2024-04-21 20:22:54 +0530

    m

---
+++ homography.py
@@ -0,0 +1,36 @@
+import numpy as np
+from ultralytics import YOLO
+import cv2
+import statistics
+
+def homography(avg_height):
+    avg_tot = []
+    f = cv2.VideoCapture('transformed_video.mp4')
+    _, image = f.read()
+    cv2.imwrite("frame.jpg",image)
+    model = YOLO('yolov8m-pose.pt')
+    results = model("frame.jpg")
+    pose=[]
+    for r in results:
+        no_of_persons = r.keypoints.xy.size(0)
+        keypoint_data = r.keypoints
+        for _ in range(int(no_of_persons)):
+            posefeats =[]
+            head_point = keypoint_data.xy[_][0]
+            right_point = keypoint_data.xy[_][16]
+            if (head_point == 0).any() or (right_point == 0).any():
+                continue
+
+            posefeats.append(head_point)
+            posefeats.append(right_point)
+            pose.append(posefeats)
+            posefeats =[]
+    cordarray = np.array(pose)
+    for x in range(len(cordarray)):
+        dist = np.linalg.norm(cordarray[x][0] - cordarray[x][1])
+        avg_tot.append(dist)
+    
+    avg=statistics.mean(avg_tot)
+    vioDist = (avg/avg_height)*1.25
+    warnDist = (avg/avg_height)*2
+    return vioDist,warnDist
---
+++ pose_extractor.py
@@ -0,0 +1,62 @@
+import pandas as pd
+from ultralytics import YOLO
+import numpy as np
+import heatmap
+import os
+
+model = YOLO('yolov8m-pose.pt')
+violationsVsFrame = []
+warningsVsFrame = []
+
+
+def main_code(violationData, warningData):
+    if(os.path.exists('file.csv')):
+        os.remove('file.csv')
+    if(os.path.exists('file2.csv')):
+        os.remove('file2.csv')
+    results = model("transformed_video.mp4", show=True,stream=True)
+    for r in results:
+        no_of_persons = r.keypoints.xy.size(0)
+
+        keypoint_data = r.keypoints
+     
+        pose=[]
+
+        posefeathead = []
+
+
+        for _ in range(int(no_of_persons)):
+            posefeats =[]
+            head_point = keypoint_data.xy[_][0]
+            right_point = keypoint_data.xy[_][16]
+            if (head_point == 0).any() or (right_point == 0).any():
+                continue
+
+            posefeats.append(head_point)
+            posefeathead.append(head_point)
+            posefeats.append(right_point)
+            pose.append(posefeats)
+            posefeats =[]
+        
+        cordarray = np.array(posefeathead)
+        print(cordarray)
+
+
+        cor = np.zeros([720,1280], dtype=int)
+        for i in range(len(cordarray)):
+            cor[int(cordarray[i][1]//1)][int(cordarray[i][0]//1)]=1
+        
+        violations, warnings=(heatmap.heatmap(cor,warningData,violationData))
+        violationsVsFrame.append(violations)
+        warningsVsFrame.append(warnings)
+        vioDict = {'vio': violationsVsFrame}
+        warnDict = {'warn':warningsVsFrame}
+        df = pd.DataFrame(vioDict)
+        df2 =pd.DataFrame(warnDict)
+        df.to_csv('file.csv')
+        df2.to_csv('file2.csv')
+
+
+           
+
+   
